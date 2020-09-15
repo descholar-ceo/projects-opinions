c7d5120a9ebddca63391b04d6cb520e4321e3443
descholar-ceo <emmanuellamugi@gmail.com>
2020-09-15 22:49:51 +0200

    Add more fields to table users

---
+++ db/migrate/20200915202725_add_more_fields_to_table_users.rb
@@ -0,0 +1,9 @@
+class AddMoreFieldsToTableUsers < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :username, :string
+    add_column :users, :fullname, :string
+    add_column :users, :photo, :string
+    add_column :users, :cover_image, :string
+    add_index :users, :username, unique: true
+  end
+end
