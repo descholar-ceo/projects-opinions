9c8636a41fd02ad6e059ec931e45dbb81cd633ad
descholar-ceo <emmanuellamugi@gmail.com>
2020-09-15 23:06:34 +0200

    Run add more fields migrations on table users

--- db/migrate/20200915202725_add_more_fields_to_table_users.rb
+++ db/migrate/20200915202725_add_more_fields_to_table_users.rb
@@ -1,7 +1,7 @@
 class AddMoreFieldsToTableUsers < ActiveRecord::Migration[6.0]
   def change
     add_column :users, :username, :string
-    add_column :users, :fullname, :string
+    add_column :users, :full_name, :string
     add_column :users, :photo, :string
     add_column :users, :cover_image, :string
     add_index :users, :username, unique: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_15_183315) do
+ActiveRecord::Schema.define(version: 2020_09_15_202725) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,8 +23,13 @@ ActiveRecord::Schema.define(version: 2020_09_15_183315) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "username"
+    t.string "full_name"
+    t.string "photo"
+    t.string "cover_image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
