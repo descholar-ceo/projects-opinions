1b52a6506d5b542828d6a09c7174a7d705fb21da
descholar-ceo <emmanuellamugi@gmail.com>
2020-09-16 21:15:39 +0200

    Styling the signin form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,10 +2,10 @@ module ApplicationHelper
   def display_user_signup_or_login
     display = ''
     if user_signed_in?
-      display = link_to('Logout', destroy_user_session_path, method: :delete, class: 'button-danger')
+      display = link_to('Logout', destroy_user_session_path, method: :delete, class: 'button-secondary')
     else
-      display << link_to('Sign In', new_user_session_path, class: 'button-info')
-      display << link_to('Sign Up', new_user_registration_path, class: 'button-info')
+      display << link_to('Sign In', new_user_session_path, class: 'button-secondary')
+      display << link_to('Sign Up', new_user_registration_path, class: 'button-secondary')
     end
     display.html_safe
   end
@@ -13,8 +13,8 @@ module ApplicationHelper
   def display_create_button
     button_display = ''
     if user_signed_in?
-      button_display << link_to('My opinions', opinions_path, class: 'button-info')
-      button_display << link_to('New opinion', new_opinion_path, class: 'button-info')
+      button_display << link_to('My opinions', opinions_path, class: 'button-secondary')
+      button_display << link_to('New opinion', new_opinion_path, class: 'button-secondary')
     end
     button_display.html_safe
   end
