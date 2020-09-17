77bd2791ac036e159ebd1ff7147e11b92c3e8029
descholar-ceo <emmanuellamugi@gmail.com>
2020-09-17 16:38:51 +0200

    Allow devise to save even the username, fullname

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :comments
   resources :followings
   resources :opinions
-  devise_for :users
+  devise_for :users, :controllers => { registrations: :registrations }
   resources :users, only: [:show,:index]
   root to: "opinions#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
