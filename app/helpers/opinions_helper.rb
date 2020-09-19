d4fdb90bbd367d9c7ffa7ac8cb46e5db57d061de
descholar-ceo <emmanuellamugi@gmail.com>
2020-09-19 10:01:13 +0200

    Fix all rubocop and stylelint lint errors after adding the rigth side bar followings and followed by

--- app/helpers/opinions_helper.rb
+++ app/helpers/opinions_helper.rb
@@ -1,9 +1,11 @@
 module OpinionsHelper
   def display_followed_by(user)
     if user.my_followers.size.positive?
-      "Followed by #{link_to user.my_followers[0].username,user.my_followers[0], class: 'text-fith-color text-small'}".html_safe
+      "Followed by #{link_to user.my_followers[0].username, user.my_followers[0], class: 'text-fith-color text-small'}"
+        .html_safe
     elsif user.followeds.size.positive?
-      "Following #{link_to user.followeds[0].username,user.followeds[0], class: 'text-fith-color text-small'}".html_safe
+      "Following #{link_to user.followeds[0].username, user.followeds[0], class: 'text-fith-color text-small'}"
+        .html_safe
     else
       'No followers, nor following'.html_safe
     end
