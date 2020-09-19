4a8a9836a5db2bed9fbfacf15bd6a8099e13129a
descholar-ceo <emmanuellamugi@gmail.com>
2020-09-19 10:13:47 +0200

    Add link class to the rigth sidebar username

--- app/helpers/opinions_helper.rb
+++ app/helpers/opinions_helper.rb
@@ -1,10 +1,10 @@
 module OpinionsHelper
   def display_followed_by(user)
     if user.my_followers.size.positive?
-      "Followed by #{link_to user.my_followers[0].username, user.my_followers[0], class: 'text-fith-color text-small'}"
+      "Followed by #{link_to user.my_followers[0].username, user.my_followers[0], class: 'link'}"
         .html_safe
     elsif user.followeds.size.positive?
-      "Following #{link_to user.followeds[0].username, user.followeds[0], class: 'text-fith-color text-small'}"
+      "Following #{link_to user.followeds[0].username, user.followeds[0], class: 'link'}"
         .html_safe
     else
       'No followers, nor following'.html_safe
