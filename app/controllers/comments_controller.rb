b0628f82bbb7d19453f4e63e17466729c0902744
descholar-ceo <emmanuellamugi@gmail.com>
2020-09-20 00:12:48 +0200

    Fix comment failing to be posted

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,34 +1,11 @@
 class CommentsController < ApplicationController
   before_action :set_comment, only: %i[show edit update destroy]
 
-  # GET /comments
-  # GET /comments.json
-  def index
-    @comments = Comment.all
-  end
-
-  # GET /comments/1
-  # GET /comments/1.json
-  def show; end
-
-  # GET /comments/new
-  def new
-    @comment = Comment.new
-  end
-
-  # GET /comments/1/edit
-  def edit; end
-
   # POST /comments
   # POST /comments.json
   def create
-    # sent_opinion_id = params[:opinion]
-    # @comment = Comment.new(comment_params)
-    # @comment.opinion = opinion
-    # @comment.user = current_user
-    @comment = current_user.comments.new(body: params[:body], opinion_id: params[:opinion])
-    
-
+    @comment = current_user.comments.new(body: params[:comment][:body], opinion_id: params[:opinion_id])
+    puts "Created comment instance is #{@comment} and params are #{params}"
     if @comment.save
       redirect_to opinions_path
     else
@@ -36,39 +13,7 @@ class CommentsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /comments/1
-  # PATCH/PUT /comments/1.json
-  def update
-    respond_to do |format|
-      if @comment.update(comment_params)
-        format.html { redirect_to @comment, notice: 'Comment was successfully updated.' }
-        format.json { render :show, status: :ok, location: @comment }
-      else
-        format.html { render :edit }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /comments/1
-  # DELETE /comments/1.json
-  def destroy
-    @comment.destroy
-    respond_to do |format|
-      format.html { redirect_to comments_url, notice: 'Comment was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
-  private
-
-  # Use callbacks to share common setup or constraints between actions.
-  def set_comment
-    @comment = Comment.find(params[:id])
-  end
-
-  # Only allow a list of trusted parameters through.
   def comment_params
-    params.require(:comment).permit(:user_id, :opinion_id, :body)
+    params.require(:comment).permit(:opinion_id, :body)
   end
 end
