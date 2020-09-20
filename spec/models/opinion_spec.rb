79a7ff1a5c22c2c631ea7e125e22a5ca3ea26d54
descholar-ceo <emmanuellamugi@gmail.com>
2020-09-20 17:18:06 +0200

    Add test of validations in opinion_spec

--- spec/models/opinion_spec.rb
+++ spec/models/opinion_spec.rb
@@ -5,4 +5,9 @@ RSpec.describe Opinion, type: :model do
     it { should belong_to(:user).with_foreign_key(:author_id) }
     it { should have_many(:comments) }
   end
+
+  describe 'Validations' do
+    it { should validate_presence_of(:text) }
+    it { should validate_length_of(:text).is_at_most(500) }
+  end
 end
