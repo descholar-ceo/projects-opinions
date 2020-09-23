899b95d6eaae201674f66fbb38d5662918828369
descholar-ceo <emmanuellamugi@gmail.com>
2020-09-23 11:25:46 +0200

    Setup resources that I am using with only: keyword

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,6 +9,8 @@ class CommentsController < ApplicationController
     end
   end
 
+  private
+  
   def comment_params
     params.require(:comment).permit(:body)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   resources :comments, only: [:create]
-  resources :followings
-  resources :opinions
+  resources :followings, only: [:create, :destroy]
+  resources :opinions, only: [:index, :create]
   devise_for :users, :controllers => { registrations: :registrations }
-  resources :users, only: [:show,:index]
+  resources :users, only: [:show, :index]
   root to: "opinions#index"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
