89dddd10d2756b8e1bd6187438bce03233ba0ea5
descholar-ceo <emmanuellamugi@gmail.com>
2020-09-23 17:57:27 +0200

    Remove cloudinary credentials and fix failing tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_16_141542) do
+ActiveRecord::Schema.define(version: 2020_09_23_152813) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,8 +51,8 @@ ActiveRecord::Schema.define(version: 2020_09_16_141542) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "username"
     t.string "full_name"
-    t.string "photo"
-    t.string "cover_image"
+    t.string "photo", default: "default_user_photo.svg"
+    t.string "cover_image", default: "default_cover.svg"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
